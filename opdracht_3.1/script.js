31070639308882ab164b4047459879350c6837fe
Bramdijke <9020733@student.zadkine.nl>
2023-02-01 00:58:51 +0100

    Opdracht 3.2 af

--- opdracht_3.1/script.js
+++ opdracht_3.1/script.js
@@ -1,8 +1,11 @@
 let budget = 100;
-let product = 60;
+
+function question() {
+    let product = prompt("Hoeveel kost het product?");
 
 if (budget >= product) {
     console.log("%cU heeft genoeg geld!","color: #42DB00;")
 } else {
     console.log("%cHelaas, te weinig geld.", "color: #DB0701;")
+}
 }
