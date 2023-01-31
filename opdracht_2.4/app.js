00b9e78a7968cc0af573aee04154afb768f39cc4
Bramdijke <9020733@student.zadkine.nl>
2023-02-01 00:03:51 +0100

    Opdracht 2.4 af

---
+++ opdracht_2.4/app.js
@@ -0,0 +1,20 @@
+// Oefening 1
+function calculator (num, num2) {
+    let answer1 = num + num2;
+    console.log(answer1);
+}
+calculator(8, 2);
+
+// Oefening 2
+function multiplyByFive (num, num2) {
+    let answer2 = num * num2;
+    console.log(answer2);
+}
+multiplyByFive(5, 6);
+
+// Oefening 3 
+function calculateMinutes (minutes) {
+    let answer3 = minutes / 60;
+    console.log(answer3);
+}
+calculateMinutes(120);
