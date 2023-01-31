920bfc47afd83414b5854ec2ee7d11ce1cef1524
Bramdijke <9020733@student.zadkine.nl>
2023-01-31 22:44:28 +0100

    Opdracht 2.2 af

---
+++ opdracht_2.2/script.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    document.getElementById("headerTitle").style.color = "teal";
+    document.getElementById("headerTitle").innerHTML = "Inhoud aangepast!";
+}
+
+// changeColor()
