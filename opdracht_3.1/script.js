0cc17dff40a0d5079d11459c17a4d2670467c46f
Bramdijke <9020733@student.zadkine.nl>
2023-02-01 00:41:15 +0100

    Opdracht 3.1 af

---
+++ opdracht_3.1/script.js
@@ -0,0 +1,8 @@
+let budget = 100;
+let product = 60;
+
+if (budget >= product) {
+    console.log("%cU heeft genoeg geld!","color: #42DB00;")
+} else {
+    console.log("%cHelaas, te weinig geld.", "color: #DB0701;")
+}
