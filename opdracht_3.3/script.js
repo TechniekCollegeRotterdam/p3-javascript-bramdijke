27d8484f24e3d46aa0d8d8c1127fc5421e0cdac2
Bramdijke <9020733@student.zadkine.nl>
2023-02-01 20:27:57 +0100

    Opdracht 3.3 af (v2)

--- opdracht_3.3/script.js
+++ opdracht_3.3/script.js
@@ -1,22 +1,14 @@
+let value = 0;
+let element = document.getElementById('increaseText');
+
 function increaseButton() {
-    let element = document.getElementById('increaseText');
-    let value = element.innerHTML;
+    if (value >= 9) {
+        value = 0;
+        element.innerText = value;
+    }
 
-    ++value;
-
-    document.getElementById('increaseText').innerHTML = value;
-
-    if (value >= 10) {
-        let value = 0;
-        document.getElementById('increaseText').innerHTML = value;
+    else {
+        value += 1;
+        element.innerText = value;
     }
 }
-
-//er zijn een aantal punten. Variabelen kun je het beste buiten de functie aanmaken. Je hebt ze straks namelijk ook nog op andere plekken nodig.
-// maak je een variabel aan IN een functie dan kan het alleen daar gebruikt worden.
-// op regel 7 doe je bijna hetzelfde als regel 2. Daar sla je namelijk je element op in de variabel element
-//dan doe je op regel 7: element.innerText = value
-//dit geldt ook voor regel 11
-
-// laaste, regel 5 is goed, maar het is beter door te zeggen: value += 1 --> dat betekend value = value + 1. Dan wordt daadwerkelijk de variabel value
-// herschreven, nu niet. dus de nieuwe "value" is de oude "value" + 1
