5766585449ad08a12eb56dec14897363862d3d7b
Jason <83275145+jschaik@users.noreply.github.com>
2023-02-01 15:39:39 +0100

    feedback 4.1

--- opdracht_4.1/script.js
+++ opdracht_4.1/script.js
@@ -1,9 +1,11 @@
 let value1 = document.getElementById("number1");
 let value2 = document.getElementById("number2");
 let solution = document.getElementById("solutionProblem");
+let answer; //maak alvast een variabel aan en laat deze leeg
 
 function multiply() {
-    let answer = Number(number1.value) * Number(number2.value);
+    answer =  Number(number1.value) * Number(number2.value); // en dan op deze manier
+//     let answer = Number(number1.value) * Number(number2.value);
     solution.innerText = "Het antwoord is " + answer;
 }
 
@@ -21,3 +23,7 @@ function minus() {
     let answer = Number(number1.value) - Number(number2.value);
     solution.innerText = "Het antwoord is " + answer;
 }
+// je bent er bijna:
+// je hebt nu overal Number() gebruikt, dat is prima, maar het was alleen nodig bij de add(). 
+// ik zie nu dat je vier keer dezelfde variabel answer aanmaakt. Als je deze ook buiten de functies plaatst hoef je het maar 1 keer aan te maken.
+// kijk op regel 4 en 7
