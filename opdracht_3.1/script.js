19cf63d4e83ee5d8ccef6e6b6cec632b2f5e8567
Bramdijke <9020733@student.zadkine.nl>
2023-02-02 10:48:02 +0100

    Opdracht 3.1 af (v2)

--- opdracht_3.1/script.js
+++ opdracht_3.1/script.js
@@ -1,11 +1,16 @@
 let budget = 100;
+let message = document.getElementById("message");
 
 function question() {
     let product = prompt("Hoeveel kost het product?");
 
 if (budget >= product) {
     console.log("%cU heeft genoeg geld!","color: #42DB00;")
+    message.innerText = "U heeft genoeg geld!";
+    message.style.color = "green";
 } else {
     console.log("%cHelaas, te weinig geld.", "color: #DB0701;")
+    message.innerText = "U heeft te weinig geld!";
+    message.style.color = "red";
 }
 }
