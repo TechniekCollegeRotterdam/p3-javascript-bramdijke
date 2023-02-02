9ddea33dd804526adfd402c1ff90eb7f41ae7394
Bramdijke <9020733@student.zadkine.nl>
2023-02-02 14:50:32 +0100

    Opdracht 5.1 incompleet

---
+++ opdracht_5.1/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('mousemove', function(e) { 
+    if (e.clientX <= 10) {
+        console.log(e.clientX);
+    }
+});
+
+
+// document.getElementById("").classList.add("show-nav");
