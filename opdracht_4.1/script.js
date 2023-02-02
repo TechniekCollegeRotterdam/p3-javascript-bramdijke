20cff48155f54a24a9e4261b5cb9e942359a59f0
Bramdijke <9020733@student.zadkine.nl>
2023-02-02 10:14:32 +0100

    Opdracht 4.2 af

--- opdracht_4.1/script.js
+++ opdracht_4.1/script.js
@@ -5,20 +5,44 @@ let answer;
 
 function multiply() {
     answer = Number(number1.value) * Number(number2.value);
-    solution.innerText = "Het antwoord is " + answer;
+    if (number1.value > 0 && number2.value > 0) {
+        solution.innerText = "Het antwoord is " + answer;
+    }
+
+    else {
+        solution.innerText = "Het is te laag";
+    }
 }
 
 function devide() {
     answer = Number(number1.value) / Number(number2.value);
-    solution.innerText = "Het antwoord is " + answer;
+    if (number1.value > 0 && number2.value > 0) {
+        solution.innerText = "Het antwoord is " + answer;
+    }
+
+    else {
+        solution.innerText = "Het is te laag";
+    }
 }
 
 function add() {
     answer = Number(number1.value) + Number(number2.value);
-    solution.innerText = "Het antwoord is " + answer;
+    if (number1.value > 0 && number2.value > 0) {
+        solution.innerText = "Het antwoord is " + answer;
+    }
+
+    else {
+        solution.innerText = "Het is te laag";
+    }
 }
 
 function minus() {
     answer = Number(value1.value) - Number(value2.value);
-    solution.innerText = "Het antwoord is " + answer;
+    if (number1.value > 0 && number2.value > 0) {
+        solution.innerText = "Het antwoord is " + answer;
+    }
+
+    else {
+        solution.innerText = "Het is te laag";
+    }
 }
