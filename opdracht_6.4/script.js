0f940e6994dbcacc906f454ca46f6616fff17655
Bramdijke <9020733@student.zadkine.nl>
2023-02-07 00:46:18 +0100

    Opdracht 6.4 af

---
+++ opdracht_6.4/script.js
@@ -0,0 +1,5 @@
+let wishlist = ["PS5", "Ferrari F8 Tributo", "Plastic lepel", "Glazen pot", "Anime figure"];
+
+for (let i = 0; i < wishlist.length; i++) {
+    document.getElementById("wishlist").innerText += wishlist[i] + "\n";
+}
