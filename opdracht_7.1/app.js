524217d4cee3f9affc70cae75a425dde925c3c52
Bramdijke <9020733@student.zadkine.nl>
2023-02-12 02:39:36 +0100

    Opdracht 7.1 bijna af

--- opdracht_7.1/app.js
+++ opdracht_7.1/app.js
@@ -2,15 +2,14 @@
 let input = document.getElementById("inputField");
 let add = document.getElementById("addToDo");
 let toDo = document.getElementById("toDoContainer");
-let addElement = document.createElement("p");
 
 add.addEventListener("click", function () {   
+    let addElement = document.createElement("p");
     let output = input.value;
+    
     addElement.innerText = output;
-
-  
-    //hier komt de code dat uitgevoerd moet worden  
-    //als er op de button wordt geklikt     
+    document.body.appendChild(addElement);
+    input.value = ""; //als er op de button wordt geklikt     
   });
 
 
