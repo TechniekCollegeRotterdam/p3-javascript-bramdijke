ff5bb7993abfd0cbbc38a127155e00bb7465faa8
Bramdijke <9020733@student.zadkine.nl>
2023-05-11 12:01:36 +0200

    Persoon object opdracht af

--- objects/books.js
+++ objects/books.js
@@ -1,8 +1,9 @@
 const book = {
     title: 'One Piece Volume 1',
     author: 'Oda Eiichiro',
-    publisher: '1997',
-    units: '35000000',
+    publisher: 'VIZ MEDIA',
+    release: 1997,
+    units: 35000000,
     price: '€9,99'
 }
 
@@ -11,6 +12,7 @@ output.innerHTML = `
 <p>${book.title}</p>
 <p>${book.author}</p>
 <p>${book.publisher}</p>
+<p>${book.release}</p>
 <p>${book.units}</p>
 <p>${book.price}</p>`
 ;
---
+++ objects/person.js
@@ -0,0 +1,30 @@
+const person = {
+  name: "Bram",
+  age: "20",
+  birth: "30-04-2003",
+  gender: "Male",
+  status: "Alive",
+  hobbies: ["Gaming", "Programming", "Drinking", "Chilling"],
+  changeName: function (name) {
+    return (this.name = name);
+  },
+  aliveStatus: function () {
+    return (person.status = "Dead");
+  },
+  addHobby: function(hobby) {
+    return this.hobbies.push(hobby) ;
+  },
+};
+person.addHobby("Designing")
+
+let output = document.querySelector(".output");
+output.innerHTML = `
+<p>${person.name}</p>
+<p>${person.age}</p>
+<p>${person.birth}</p>
+<p>${person.gender}</p>
+<p>${person.status}</p>
+<p>${person.hobbies.join(", ")}</p>
+<p>${person.changeName("Ronald")}</p>
+<p>${person.aliveStatus()}</p>
+<p>${person.hobbies[4]}</p>`;
