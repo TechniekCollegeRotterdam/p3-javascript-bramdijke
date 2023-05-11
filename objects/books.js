9b7a9124ec67a7f3dbdecb5643ec9775dcecc025
Bramdijke <9020733@student.zadkine.nl>
2023-05-11 11:02:07 +0200

    Boek object opdracht af

---
+++ objects/books.js
@@ -0,0 +1,16 @@
+const book = {
+    title: 'One Piece Volume 1',
+    author: 'Oda Eiichiro',
+    publisher: '1997',
+    units: '35000000',
+    price: '€9,99'
+}
+
+let output = document.querySelector('.output');
+output.innerHTML = `
+<p>${book.title}</p>
+<p>${book.author}</p>
+<p>${book.publisher}</p>
+<p>${book.units}</p>
+<p>${book.price}</p>`
+;
